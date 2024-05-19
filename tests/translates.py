7a46e9aa0e1619ed6aaed464777aa9b3c2e58959
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-05-19 20:48:54 +0800

    Updated translates.py

--- tests/translates.py
+++ tests/translates.py
@@ -1,9 +1,14 @@
 import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+ 
+RAPID_API_KEY = os.getenv('RAPID_API')
 url = "https://deep-translate1.p.rapidapi.com/language/translate/v2/languages"
 
 headers = {
-	"X-RapidAPI-Key": "defb4f53a9msh0dc05b3f255fab2p16601ejsndefc49120355",
+	"X-RapidAPI-Key": {RAPID_API_KEY},
 	"X-RapidAPI-Host": "deep-translate1.p.rapidapi.com"
 }
 
